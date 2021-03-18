ea854d9e842ef91a48d4924443399f60361894f8
nomzamo <zamapm@gmail.com>
2021-03-18 09:50:47 +0200

    remove annotation

--- src/main/java/za/co/entelect/invest.easy.dojo/messaging/InvestEasyApplication.java
+++ src/main/java/za/co/entelect/invest.easy.dojo/messaging/InvestEasyApplication.java
@@ -2,13 +2,10 @@ package za.co.entelect.invest.easy.dojo.messaging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
 
 //TODO: 1. Add @EnableJms annotation for the discovery of the methods with @JmsListener
 @SpringBootApplication
-@EnableJms
 public class InvestEasyApplication {
-
     public static void main(String[] args){
         SpringApplication.run(InvestEasyApplication.class, args);
     }
