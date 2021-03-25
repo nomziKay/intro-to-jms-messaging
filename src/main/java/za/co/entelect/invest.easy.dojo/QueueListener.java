0f602ebb5585c44f14ca5cc3040a1a102eed8adc
rendani <rendanid@discovery.co.za>
2021-03-25 21:10:52 +0200

    Init JMS Exercise 1 complete

--- src/main/java/za/co/entelect/invest.easy.dojo/QueueListener.java
+++ src/main/java/za/co/entelect/invest.easy.dojo/QueueListener.java
@@ -1,9 +1,8 @@
 package za.co.entelect.invest.easy.dojo;
 
-import javax.jms.Connection;
-import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
-import javax.jms.Session;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
 
 public class QueueListener {
 
@@ -11,22 +10,47 @@ public class QueueListener {
 
     private final MessageConsumer messageConsumer;
 
-    private final Session session;
-
     public QueueListener() throws JMSException {
         System.out.println("Initializing JMS connection");
+        //ActiveMQConnectionFactory.DEFAULT_BROKER_URL = tcp://localhost:61616
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnectionFactory.DEFAULT_BROKER_URL);
+        connection = connectionFactory.createConnection();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        //Initialize JMS Connection
+        Destination readDestination = session.createQueue("Q.za.co.entelect.dojo.jms");
+
+        messageConsumer = session.createConsumer(readDestination);
+
+        connection.start();
 
         System.out.println("Initialization Complete");
     }
 
     public void listen() throws JMSException {
+        Message message = null;
+        while (true) {
+            System.out.println("Listening for message...");
+            message = messageConsumer.receive(20000L);
 
+            if (message == null) {
+                System.out.println("Didn't read anything from queue in 20 seconds. Terminating");
+                break;
+            }
+
+            if (!(message instanceof TextMessage)) {
+                System.out.println("Not a TextMessage, discarding");
+                continue;
+            }
+
+            //process message
+            System.out.println(message.getJMSMessageID());
+            System.out.println(((TextMessage) message).getText());
+        }
     }
 
     public void closeConnection() throws JMSException {
         System.out.println("Closing JMS connection");
+        connection.close();
     }
 
     public static void main(String[] args) throws JMSException {
