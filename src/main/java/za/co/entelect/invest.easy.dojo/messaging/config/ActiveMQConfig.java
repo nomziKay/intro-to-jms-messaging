1fbb5d7e90c5393f8b8a6d0e294300145c3736bd
rendani <rendanid@discovery.co.za>
2021-03-29 20:59:44 +0200

    init prac 2

--- src/main/java/za/co/entelect/invest.easy.dojo/messaging/config/ActiveMQConfig.java
+++ src/main/java/za/co/entelect/invest.easy.dojo/messaging/config/ActiveMQConfig.java
@@ -9,24 +9,23 @@ import org.springframework.jms.core.JmsTemplate;
 public class ActiveMQConfig {
 
     //Please note: This is not required when using SpringBoot; we will explicitly define the beans here to demo how it should be done when using a traditional Spring application
-    //We will only define the jmsTemplate moving forward as we need to add some of our own properties to it.
 
     //Store these in a seperate config file/app
     String URL = "tcp://localhost:61616";
     String USERNAME = "admin";
     String PASSWORD = "admin";
 
-    //TODO: 1. Create the connectionFactory
     //This will be an activeMQ connectionFactory since our message broker is ActiveMQ
     @Bean
     public ActiveMQConnectionFactory connectionFactory(){
         return new ActiveMQConnectionFactory(URL,USERNAME,PASSWORD);
     }
 
-    //TODO: 2. Create a jmsTemplate bean named changeNotificationJmsTemplate
     @Bean
     public JmsTemplate changeNotificationJmsTemplate(){
         return new JmsTemplate(connectionFactory());
     }
+
+    //TODO: 1. Create the jmsTemplate to publish to a Topic
 
 }
