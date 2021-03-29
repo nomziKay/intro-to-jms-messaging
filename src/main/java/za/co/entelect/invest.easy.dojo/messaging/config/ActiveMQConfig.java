d6b7cab90dec4c0a09e044f9496515cf3bf0d9f4
rendani <rendanid@discovery.co.za>
2021-03-29 21:43:49 +0200

    prac 2 complete

--- src/main/java/za/co/entelect/invest.easy.dojo/messaging/config/ActiveMQConfig.java
+++ src/main/java/za/co/entelect/invest.easy.dojo/messaging/config/ActiveMQConfig.java
@@ -4,6 +4,10 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.JMSException;
 
 @Configuration
 public class ActiveMQConfig {
@@ -17,15 +21,32 @@ public class ActiveMQConfig {
 
     //This will be an activeMQ connectionFactory since our message broker is ActiveMQ
     @Bean
-    public ActiveMQConnectionFactory connectionFactory(){
-        return new ActiveMQConnectionFactory(URL,USERNAME,PASSWORD);
+    public ActiveMQConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory(URL, USERNAME, PASSWORD);
     }
 
     @Bean
-    public JmsTemplate changeNotificationJmsTemplate(){
+    public JmsTemplate changeNotificationJmsTemplate() {
         return new JmsTemplate(connectionFactory());
     }
 
     //TODO: 1. Create the jmsTemplate to publish to a Topic
+    @Bean
+    public MappingJackson2MessageConverter jacksonMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    //We explicitly define connection factory so we can set the PubSubDomain property
+    @Bean(name = "easyInvestJmsTemplate")
+    public JmsTemplate jmsTemplate() throws JMSException {
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(connectionFactory());
+        jmsTemplate.setPubSubDomain(true);  // enable for Pub Sub to topic. Not Required for Queue.
+        jmsTemplate.setMessageConverter(jacksonMessageConverter());
+        return jmsTemplate;
+    }
 
 }
---
+++ src/main/java/za/co/entelect/invest.easy.dojo/messaging/domain/Constants.java
@@ -0,0 +1,5 @@
+package za.co.entelect.invest.easy.dojo.messaging.domain;
+
+public class Constants {
+    public static final String NOTIFICATION_TOPIC = "T.za.co.easyinvest.share.notification";
+}
